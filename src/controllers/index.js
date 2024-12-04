ed45b1a9646d185f28613628a8f72752ff2b7dbb
Omkar Raykar <raykaromkar1244@gmail.com>
2024-12-04 20:55:42 +0530

    added some files for deployment purpose on netlify

---
+++ src/controllers/index.js
@@ -0,0 +1,3 @@
+export { default as userController } from './userController.js';
+export { default as authController } from './authController.js';
+export { default as facilityController } from './facilityController.js';
