cc4eda11d9ef6eb3f913d33de5ea78baed4b2317
Omkar Raykar <raykaromkar1244@gmail.com>
2025-03-06 19:55:48 +0530

    done with hospital model

--- src/controllers/hospitalController.js
+++ src/controllers/hospitalController.js
@@ -17,9 +17,12 @@ const createHospital = asyncHandler(async (req, res) => {
   const { name, address, specializedIn, contactNumber } = req.body;
 
   //validate user role
-  const allowedRoles = ["Admin"]
+  const allowedRoles = [0];
   if (!allowedRoles.includes(req.user.role)) {
-    throw new ApiError(403, "You are not authorized to create a hospital");
+    return res.status(403).json({
+      success: false,
+      message: "Access denied: Unauthorized to access this resource",
+    });
   }
 
   // validate required fields
@@ -31,7 +34,10 @@ const createHospital = asyncHandler(async (req, res) => {
     !specializedIn ||
     !contactNumber
   ) {
-    throw new ApiError(400, "All required fields must be provided");
+    return res.status(403).json({
+      success: false,
+      message: "All fields are required",
+    });
   }
 
   // create the hospital
@@ -39,105 +45,160 @@ const createHospital = asyncHandler(async (req, res) => {
     name,
     address,
     specializedIn,
-    contactNumber
-  })
+    contactNumber,
+  });
   // respond with the created hospital
   return res
     .status(201)
     .json(new ApiResponse(201, hospital, "Hospital created successfully"));
-
 });
 
 // @desc     fetch all hospital from db (role based fetching)
 // route     GET api/v1/hospitals/getAllHospital
 // @accesss  Private
 
-const getAllHospital = asyncHandler(async(req,res)=>{
+const getAllHospital = asyncHandler(async (req, res) => {
   // fetch the role of the requesting user
-  const requestingUser = await Hospital.findById(req.user._id)
-  if(!requestingUser){
-    throw new ApiError(404, "Requesting user not found")
+  const requestingUser = await Hospital.findById(req.user._id);
+  if (!requestingUser) {
+    return res.status(404).json({
+      success: false,
+      message: "Requesting user not found",
+    });
   }
 
-  let roleFilter = {};
+  const { role } = requestingUser;
 
-  if (!requestingUser.role === "Admin" || "Hospital") {
-    throw new ApiError(401, "Unauthorized to access the resource");
+  if (![0, 1, 2, 3].includes(role)) {
+    return res.status(403).json({
+      success: false,
+      message: "Access denied: Unauthorized to access this resource",
+    });
   }
   // Fetch users based on the role filter, active status, and non-deleted status
-    const users = await Hospital.find({
-      ...roleFilter,
-      isActive: true,
-      isDeleted: false,
-    });
-    // Return the fetched users
-    res
-      .status(200)
-      .json(new ApiResponse(200, "Hospital fetched successfully", users));
-})
+  const hospitals = await Hospital.find({
+    isActive: true,
+    isDeleted: false,
+  });
+  // Return the fetched users
+  return res.status(200).json({
+    success: true,
+    message: "Hospitals fetched successfully",
+    data: hospitals,
+  });
+});
 
 // @desc     fetch a hospital by Id from db
 // route     GET api/v1/hospitals/{id}
 // @accesss  Private
-const getHospitalById = asyncHandler(async(req,res)=>{
-  const {hospitalId} = req.params;
+const getHospitalById = asyncHandler(async (req, res) => {
+  const { hospitalId } = req.params;
 
   // validate hospitalId parameter
   if (!isValidObjectId(hospitalId)) {
-    throw new ApiError(400, "Invalid hospital ID");
+    return res.status(400).json({
+      success: false,
+      message: "Invalid user ID",
+    });
   }
 
-  const hospital = await Hospital.findById(hospitalId)
+  const hospital = await Hospital.findById(hospitalId);
   //check if the hospital was found
   if (!hospital) {
-    throw new ApiError(404, "hospital not found");
+    return res.status(404).json({
+      success: false,
+      message: "Requesting hospital not found",
+    });
   }
 
   // return the hospital details
   return res
     .status(200)
     .json(new ApiResponse(200, hospital, "Facility fetched successfully"));
-})
+});
 
 // @desc     update hospital by Id from db (role based)
 // route     PATCH api/v1/hospitals/{id}
 // @accesss  Private
-const updateHospital= asyncHandler(async (req, res) => {
+const updateHospital = asyncHandler(async (req, res) => {
   const { hospitalId } = req.params;
   const { name, address, specializedIn, contactNumber } = req.body;
 
   // validate hospital
   if (!isValidObjectId(hospitalId)) {
-    throw new ApiError(400, "Invalid hospital ID");
+    return res.status(400).json({
+      success: false,
+      message: "Invalid hospital ID",
+    });
   }
-  // Prepare the update object
-  const updateData = {};
-  if (name) updateData.name = name;
-  if (address) updateData.address = address;
-  if (specializedIn) updateData.specializedIn = specializedIn;
-  if (contactNumber) updateData.contactNumber = contactNumber;
- 
 
-  // update user
-  const updatedHospital = await Hospital.findByIdAndUpdate(
-    hospitalId,
-    updateData,
-    {
-      new: true,
+  try {
+    // Get the requesting user's details
+    const requestingUser = await User.findById(req.user._id);
+
+    if (!requestingUser) {
+      return res.status(404).json({
+        success: false,
+        message: "Requesting user not found",
+      });
     }
-  );
+    // Check if the requesting user has role 0 (Admin)
+    if (![0].includes(requestingUser.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied: Unauthorized to update this hospital",
+      });
+    }
+    // Retrieve the existing facility
+    const existingHospital = await Hospital.findById(hospitalId);
+    if (!existingHospital) {
+      return res.status(404).json({
+        success: false,
+        message: "Hospital not found",
+      });
+    }
+    // Prepare the update object
+    const updateData = {};
+    if (name) updateData.name = name;
+    if (address) {
+      updateData.address = {
+        state: address.state || existingHospital.address.state,
+        city: address.city || existingHospital.address.city,
+        pincode: address.pincode || existingHospital.address.pincode,
+      };
+    }
+    if (specializedIn) updateData.specializedIn = specializedIn;
+    if (contactNumber) updateData.contactNumber = contactNumber;
 
-  // check if hospital was found and update
-  if (!updatedHospital) {
-    throw new ApiError(404, "hospital not found");
-  }
-
-  // return the updated hospital details
-  return res
-    .status(200)
-    .json(
-      new ApiResponse(200, updatedHospital, "hospital updated successfully")
+    // update user
+    const updatedHospital = await Hospital.findByIdAndUpdate(
+      hospitalId,
+      updateData,
+      {
+        new: true,
+      }
     );
+
+    // check if hospital was found and update
+    if (!updatedHospital) {
+      return res.status(404).json({
+        success: false,
+        message: "Hospital not found",
+      });
+    }
+
+    // return the updated hospital details
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, updatedFacility, "Facility updated successfully")
+      );
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while updating the hospital",
+    });
+  }
 });
 
 // @desc     delete hospital by Id from db (soft-delete & role based)
@@ -148,30 +209,59 @@ const deleteHospital = asyncHandler(async (req, res) => {
 
   // validate the hospitalId
   if (!isValidObjectId(hospitalId)) {
-    throw new ApiError(400, "Invalid hospital ID");
+    return res.status(400).json({
+      success: false,
+      message: "Invalid hospital ID",
+    });
   }
+  try {
+    // Get the requesting user's details
+    const requestingUser = await User.findById(req.user._id);
 
-  // Delete the hospital (soft delete)
-  const hospital = await Hospital.findByIdAndUpdate(
-    hospitalId,
-    { isDeleted: true },
-    { new: true }
-  );
+    if (!requestingUser) {
+      return res.status(404).json({
+        success: false,
+        message: "Requesting user not found",
+      });
+    }
 
-  if (!hospital) {
-    throw new ApiError(404, "Hospital not found");
+    // Check if the requesting user has role 0 (Admin)
+    if (![0].includes(requestingUser.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied: Unauthorized to delete this user",
+      });
+    }
+    // Delete the hospital (soft delete)
+    const hospital = await Hospital.findByIdAndUpdate(
+      hospitalId,
+      { isDeleted: true },
+      { new: true }
+    );
+
+    if (!hospital) {
+      return res.status(404).json({
+        success: false,
+        message: "Hospital not found",
+      });
+    }
+
+    //return success message
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "", "Hospital deleted successfully"));
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while deleting the hospital",
+    });
   }
-
-  //return success message
-  return res
-    .status(200)
-    .json(new ApiResponse(200, "", "Hospital deleted successfully"));
 });
 
 export {
- createHospital,
- getAllHospital,
- getHospitalById,
- updateHospital,
- deleteHospital
+  createHospital,
+  getAllHospital,
+  getHospitalById,
+  updateHospital,
+  deleteHospital,
 };
